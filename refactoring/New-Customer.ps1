c143b410bcd3a4b6249646a72657213c0f5265fe
Scott McBain <rsmcbain@gmail.com>
2016-11-03 17:09:08 -0500

    replace temp variable with query

--- refactoring/New-Customer.ps1
+++ refactoring/New-Customer.ps1
@@ -10,7 +10,7 @@
     $c.PSObject.TypeNames.Insert(0,'Customer')
     $c | Add-Member -MemberType ScriptMethod -Name calcStatement -Value {
         $result = $null
-        $total_amount, $frequent_renter_points = 0,0
+        $total_amount, $frequent_renter_points = 0, 0
         $result = "Rental record for {0}`r`n" -f $this.Name
         foreach ($rental in $this.Rentals) {
             # calc frequent renter points
@@ -19,7 +19,7 @@
                 $frequent_renter_points += 1
             }
             $total_amount += $rental.charge
-            $result += "`t{0}`t{1}`r`n" -f $rental.Movie.Title, $this_amount
+            $result += "`t{0}`t{1}`r`n" -f $rental.Movie.Title, $rental.charge
         }
     $result += "Amount owed is {0}`n`r" -f $total_amount
     $result += "You have earned {0} frequent renter points`n`r" -f $frequent_renter_points
@@ -27,4 +27,3 @@
     }
     $c
 }
-
