cbc94612708072f78443430fa6456bb5f2931884
Scott McBain <rsmcbain@gmail.com>
2016-11-03 16:36:15 -0500

    extract amount_for method locally

--- refactoring/New-Customer.ps1
+++ refactoring/New-Customer.ps1
@@ -13,37 +13,45 @@
         $total_amount, $frequent_renter_points = 0,0
         $result = "Rental record for {0}`r`n" -f $this.Name
         foreach ($rental in $this.Rentals) {
-            $this_amount = 0
-            # calc amounts for each line
-            switch ($rental.Movie.PriceCode) {
-                0 { 
-                    $this_amount += 2 
-                    if ($rental.DaysRented > 2) {
-                        $this_amount += ($rental.DaysRented -2 ) *1.5
-                    }
-                }
-                1 { 
-                    $this_amount += $rental.DaysRented * 3
-                }
-                2 {
-                  $this_amount += 1.5
-                  if ($rental.DaysRented > 3) {
-                      $this_amount += ($rental.DaysRented -3 ) *1.5
-                  }
-                }
-                default { 0 }
-            }
             # calc frequent renter points
             $frequent_renter_points += 1
             if ($rental.Movie.PriceCode = 1) {
                 $frequent_renter_points += 1
             }
-            $total_amount += $this_amount
+            $total_amount += $this.amountFor($rental)
             $result += "`t{0}`t{1}`r`n" -f $rental.Movie.Title, $this_amount
         }
     $result += "Amount owed is {0}`n`r" -f $total_amount
     $result += "You have earned {0} frequent renter points`n`r" -f $frequent_renter_points
     "$result"
+    }
+    $c | Add-Member -MemberType ScriptMethod -Name amountFor -Value {
+        params (
+            [Parameter(Mandatory=$true)]
+            [ValidateScript({$_.PSObject.TypeNames[0] -eq 'Rental' })]
+            $rental
+        )
+        $this_amount = 0
+        # calc amounts for each line
+        switch ($rental.Movie.PriceCode) {
+            0 { 
+                $this_amount += 2 
+                if ($rental.DaysRented > 2) {
+                    $this_amount += ($rental.DaysRented -2 ) *1.5
+                }
+            }
+            1 { 
+                $this_amount += $rental.DaysRented * 3
+            }
+            2 {
+                $this_amount += 1.5
+                if ($rental.DaysRented > 3) {
+                    $this_amount += ($rental.DaysRented -3 ) *1.5
+                }
+            }
+            default { 0 }
+        }
+        $this_amount
     }
     $c
 }
